86423e1f00853263892c253e1f17a01e1e1bca94
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-22 12:54:32 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,22 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_support = true
+
+  tags = merge({
+    Name = "${var.env}-vpc"
+  },
+    var.tags)
+}
+
+module "subnets" {
+  source = "./subnets"
+
+  for_each = var.subnets
+  cidr_block = eacn.value["cidr_block"]
+  subnet_name = each.key
+
+  vpc_id = aws_vpc.main.id
+
+  env  = var.env
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
 variable "cidr_block" {}
+variable "env" {}
+variable "tags" {}
+variable "subnets" {}
