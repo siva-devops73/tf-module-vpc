4a3ebb6c11a22662aee4090f7a29833ef3ea3bd7
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-22 13:15:21 +0530

    roboshop-terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -4,6 +4,6 @@ resource "aws_subnet" "main" {
   cidr_block = element(var.cidr_block, count.index)
 
   tags = merge({
-    Name = "${var.env}-$(var.subnet_name)-subnet"},
+    Name = "${var.env}-${var.subnet_name}-subnet"},
     var.tags)
 }
