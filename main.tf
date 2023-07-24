c359564b2f8e524d104529846d6fe5dece1de169
Siva Kumar <sivakumar9399959803@gmail.com>
2023-07-24 15:51:27 +0530

    roboshop-terraform

--- main.tf
+++ main.tf
@@ -39,6 +39,6 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_route" "route-igw" {
   route_table_id            = module.subnets["public"].route_table_ids
-  destination_cidr_block    = "10.0.0.0/0"
+  destination_cidr_block    = "0.0.0.0/0"
   gateway_id               = aws_internet_gateway.igw.id
 }
